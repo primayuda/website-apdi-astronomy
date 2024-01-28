7c2ed6b0e403d9f0b66f2eb901b37fa15668951c
Aprita Primayuda <apritaprimayuda@saris-MacBook-Pro.local>
2024-01-28 13:12:12 +0700

    Animnation with AlpineJS on Stat at Index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,22 +5,21 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import vercel from '@astrojs/vercel/static';
 
+import alpinejs from "@astrojs/alpinejs";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-nomy.vercel.app",
-  integrations: [
-    mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'github-dark-dimmed' },
-      gfm: true,
-    }),
-    sitemap(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [mdx({
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'github-dark-dimmed'
+    },
+    gfm: true
+  }), sitemap(), react(), tailwind({
+    applyBaseStyles: false
+  }), alpinejs({ entrypoint: '/src/entrypoint' })],
   adapter: vercel({
-    analytics: true,
-  }),
+    analytics: true
+  })
 });
